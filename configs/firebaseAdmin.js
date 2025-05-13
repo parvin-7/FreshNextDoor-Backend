42273a57cbcac1ae8972e7c1f93cd42d442d7d3a
parvindar <parvindarsingh43958@gmail.com>
2025-05-14 00:08:51 +0530

    update

--- configs/firebaseAdmin.js
+++ configs/firebaseAdmin.js
@@ -2,6 +2,10 @@
 import admin from "firebase-admin";
 
 // Load JSON from env
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT env variable not set');
+}
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
